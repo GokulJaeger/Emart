e844a8e47b4a96e97e53de59b63f06cfb4ed062b
GokulJaeger <gokuljaeger@gmail.com>
2021-05-18 11:55:40 +0530

    first commit

---
+++ SSE/EMart/src/main/java/com/inventory/emart/entity/Warehouse.java
@@ -0,0 +1,11 @@
+package com.inventory.emart.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "warehouse")
+public class Warehouse {
+    
+    
+}
